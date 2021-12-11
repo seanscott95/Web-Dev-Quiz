80eaf9377afea91082f7ed1c7083f6106129d971
Sean <seanscott95@live.com>
2021-12-11 20:21:55 +1100

    started js, added event listener for start button

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+// start button click starts game
+// questions appear
+// timer starts
+// if answer is correct show correct else show wrong
+// if answer is wrong penalise 10s
+// go to next question
+// repeat for all questions
+// if either all questions answered or if timer runs out end game and go to form to input score
+// after input go to highscores
+// show highscores if high enough time left
+// have play again button
+
+var startButton = document.querySelector("#start-btn");
+
+startButton.addEventListener("click", startGame);
+
+
+
+function startGame() {
+    console.log("STarted")
+}
+
+function selectNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
